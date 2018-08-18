dc44f97a50632bba285b9b94898b7bd0785c3aa5
mjguru <mjgurumukhani@gmail.com>
2018-08-18 00:02:13 -0700

    Initial Commit

---
+++ Duplicator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Duplicator
+//
+//  Created by Mohit on 18/08/18.
+//  Copyright © 2018 Mohit. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Duplicator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Duplicator
+//
+//  Created by Mohit on 18/08/18.
+//  Copyright © 2018 Mohit. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
