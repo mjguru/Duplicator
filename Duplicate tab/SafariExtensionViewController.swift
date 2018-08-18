0b870d7baf8f298c85dc454e3cffb03c496c3047
mjguru <mjgurumukhani@gmail.com>
2018-08-18 00:03:55 -0700

    Init

---
+++ Duplicate tab/SafariExtensionViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SafariExtensionViewController.swift
+//  Duplicate tab
+//
+//  Created by Mohit on 18/08/18.
+//  Copyright © 2018 Mohit. All rights reserved.
+//
+
+import SafariServices
+
+class SafariExtensionViewController: SFSafariExtensionViewController {
+    
+    static let shared = SafariExtensionViewController()
+
+}
